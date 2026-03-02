b278114182eda304264d739b5c02eaebfcae20bc
Dopester03 <soumiksutradhar0612@gmail.com>
2026-03-03 03:26:37 +0530

    App displays version and cluster awareness

--- app.py
+++ app.py
@@ -4,12 +4,17 @@ import os
 
 app = Flask(__name__)
 
+APP_VERSION = os.getenv("VERSION", "v1")
+IN_K8S = os.getenv("KUBERNETES_SERVICE_HOST") is not None
+
 @app.route("/")
 def home():
     return f"""
-    <h2>Kubernetes Web App</h2>
+    <h2>ClusterVitals - Kubernetes Deployment Monitor</h2>
     <p><b>Hostname:</b> {socket.gethostname()}</p>
     <p><b>Environment:</b> {os.getenv('ENV','dev')}</p>
+    <p><b>Version:</b> {APP_VERSION}</p>
+    <p><b>Running Inside Kubernetes:</b> {"Yes" if IN_K8S else "No"}</p>
     """
 
 if __name__ == "__main__":
